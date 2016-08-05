aeb78d1a5eea1c1424cc31877a8c83db4866957f
Johan Compagner <jcompagner@servoy.com>
2016-08-05 12:30:55 +0200

    SVY-10216 FormComponent in responsive palette
    added a form component component that can hold a absolute or a relative
    form in a main relative form
    If the form is absolute then you have to set at least the height

---
+++ formcomponent/formcomponent.js
@@ -0,0 +1,53 @@
+angular.module('bootstrapcomponentsFormcomponent',['servoy']).directive('bootstrapcomponentsFormcomponent', ['$compile',function($compile) {
+    return {
+           restrict : 'E',
+           scope : {
+        	   model: '=svyModel',
+        	   api : "=svyApi",
+        	   svyServoyapi: "=",
+        	   handlers: "=svyHandlers"
+           },
+           controller: function($scope, $element, $attrs)
+           {
+        	   function createContent() {
+        		   $element.empty();
+        		   var newValue = $scope.model.containedForm;
+        		   if (newValue) {
+        			   var elements = $scope.svyServoyapi.getFormComponentElements("containedForm", newValue);
+        			   if ($scope.model.height || $scope.model.width) {
+        				   var template = "<div style='";
+        				   if ($scope.model.height) template += "height:" +$scope.model.height + "px;"
+        				   if ($scope.model.width) template += "width:" +$scope.model.width + "px;"
+        				   template += "'></div>";
+        				   var div = $compile(template)($scope);
+        				   div.append(elements);
+        				   $element.append(div);
+        			   }
+        			   else $element.append(elements);
+        		   }
+        		   else {
+        			   $element.html("<div>FormComponent, select a form</div>");
+        		   }
+        	   }
+        	   $scope.$watch("model.containedForm", function() { 
+        		  createContent();
+        	   });
+        	   if ($scope.svyServoyapi.isInDesigner()) {
+        		   var previousWidth = $scope.model.width;
+        		   var previousHeigth = $scope.model.height;
+        		   $scope.$watch("model.width", function() { 
+        			  if (previousWidth != $scope.model.width) {
+	             		  createContent();
+	             		  previousWidth = $scope.model.width;
+        			  }
+             	   });
+        		   $scope.$watch("model.height", function() { 
+         			  if (previousHeigth != $scope.model.height) {
+                  		  createContent();
+                  		  previousHeigth = $scope.model.height;
+        			  }
+              	   });
+        	   }
+        }
+    }
+}])
---
+++ formcomponent/formcomponent.spec
@@ -0,0 +1,14 @@
+{
+	"name": "bootstrapcomponents-formcomponent",
+	"displayName": "FormComponent",
+	"version": 1,
+	"icon": "bootstrapcomponents/formcomponent/form.gif",
+	"definition": "bootstrapcomponents/formcomponent/formcomponent.js", 
+	"libraries": [],	
+	"model":
+	{
+		"containedForm": "formcomponent",
+		"width" : {"type" :"int", "tags": { "scope" :"design" }},
+		"height" :{"type" :"int", "tags": { "scope" :"design" }}
+	}
+}
