d8a078b1a3c2ba29d8f9fe52c220ece65bbd540a
emera <emera@servoy.com>
2016-08-24 18:03:44 +0300

    SVY-10228 bootstrap typeahead and select component should use the new valuelist.getDisplayValue() api

--- select/select.js
+++ select/select.js
@@ -14,3 +14,39 @@ angular.module('bootstrapcomponentsSelect',['servoy']).directive('bootstrapcompo
       templateUrl: 'bootstrapcomponents/select/select.html'
     };
 })
+.filter('showDisplayValue', function () { // filter that takes the realValue as an input and returns the displayValue
+	return function (input, valuelist) {
+		var i = 0;
+		var realValue = input;
+		if (valuelist) {
+			if (input && input.hasOwnProperty("realValue")) {
+				realValue = input.realValue;
+			}
+			//TODO performance upgrade: change the valuelist to a hashmap so that this for loop is no longer needed. 
+			//maybe to something like {realValue1:displayValue1, realValue2:displayValue2, ...}
+			for (i = 0; i < valuelist.length; i++) {
+				if (realValue === valuelist[i].realValue) {
+					return valuelist[i].displayValue;
+				}
+			}
+			var hasRealValues = false;
+			for (var i = 0; i < valuelist.length; i++) {
+				var item = valuelist[i];
+				if (item.realValue != item.displayValue) {
+					hasRealValues = true;
+					break;
+				}
+			}
+			if (hasRealValues) {
+				var diplayValue = null;
+				// this then function will resolve right away if the value is already cached on the client.
+				valuelist.getDisplayValue(realValue).then(function(val){
+					diplayValue = val;
+				})
+				return diplayValue;
+			}
+			if (valuelist.length == 0) return null;
+		}
+		return input;
+	};
+});
--- typeahead/typeahead.js
+++ typeahead/typeahead.js
@@ -1,4 +1,4 @@
-angular.module('bootstrapcomponentsTypeahead', ['servoy']).directive('bootstrapcomponentsTypeahead', ['formatFilterFilter', function(formatFilter) {
+angular.module('bootstrapcomponentsTypeahead', ['servoy']).directive('bootstrapcomponentsTypeahead', ['formatFilterFilter','$q', function(formatFilter,$q) {
   return {
     restrict: 'E',
     scope: {
@@ -9,36 +9,48 @@ angular.module('bootstrapcomponentsTypeahead', ['servoy']).directive('bootstrapc
     },
     link: function($scope, $element) {
 
+      $scope.ngModel = $element.controller('ngModel');
+    	
       $scope.formatLabel = function(model) {
         var displayFormat = undefined;
         var type = undefined;
         var displayValue = null;
-        if ($scope.model.valuelistID) {
+        if ($scope.model.valuelistID && $scope.model.valuelistID.length > 0 && $scope.model.valuelistID[0].displayValue) {
+          var found = false;
+          var realValue = typeof $scope.model.valuelistID[0].realValue == "number" && !isNaN(Number($scope.ngModel.$modelValue)) ? Number($scope.ngModel.$modelValue) : $scope.ngModel.$modelValue;
           for (var i = 0; i < $scope.model.valuelistID.length; i++) {
-            if (model === $scope.model.valuelistID[i].realValue) {
+            if (realValue === $scope.model.valuelistID[i].realValue) {
               displayValue = $scope.model.valuelistID[i].displayValue;
+              found = true;
               break;
             }
+          }
+          if (!found && typeof realValue === typeof $scope.model.valuelistID[0].realValue)
+          {
+        	  $scope.model.valuelistID.getDisplayValue(realValue).then(function(dispValue){
+        		  displayValue = dispValue;
+        	  });       	  
           }
         } else {
           displayValue = model;
         }
         if ($scope.model.format && $scope.model.format.display) displayFormat = $scope.model.format.display;
         if ($scope.model.format && $scope.model.format.type) type = $scope.model.format.type;
-        return formatFilter(displayValue, displayFormat, type);
+    	return formatFilter(displayValue, displayFormat, type);
       }
       $scope.doSvyApply = function() {
-        if (angular.element('[typeahead-popup]').attr('aria-hidden') == "true") {
+         if (angular.element('[typeahead-popup]').attr('aria-hidden') == "true") {
           if ($scope.model.valuelistID && $scope.model.valuelistID.length > 0 && $scope.model.valuelistID[0].displayValue) {
             var hasMatchingDisplayValue = false;
             for (var i = 0; i < $scope.model.valuelistID.length; i++) {
               if ($element.val() === $scope.model.valuelistID[i].displayValue) {
+            	$scope.model.dataProviderID = $scope.model.valuelistID[i].realValue;
                 hasMatchingDisplayValue = true;
                 break;
               }
             }
             if (!hasMatchingDisplayValue) {
-              $scope.model.dataProviderID = null;
+            	$scope.model.dataProviderID = null;
             }
           }
           $scope.svyServoyapi.apply('dataProviderID');
