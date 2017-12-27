b66bda80ac2b96540d4968a68c9bccb43f8b61af
Paolo <paronne@servoy.com>
2017-12-27 16:56:43 +0200

    SVY-11846 : change styleClass of form component container at runtime (for absolute forms only !!)

--- formcomponent/formcomponent.js
+++ formcomponent/formcomponent.js
@@ -1,4 +1,4 @@
-angular.module('bootstrapcomponentsFormcomponent',['servoy']).directive('bootstrapcomponentsFormcomponent', ['$compile',function($compile) {
+angular.module('bootstrapcomponentsFormcomponent',['servoy']).directive('bootstrapcomponentsFormcomponent', ['$compile', '$sabloConstants',function($compile, $sabloConstants) {
     return {
            restrict : 'E',
            scope : {
@@ -25,12 +25,13 @@ angular.module('bootstrapcomponentsFormcomponent',['servoy']).directive('bootstr
         				   if (height) template += "height:" +height + "px;"
         				   if (width) template += "width:" +width + "px;"
         				   template += "'";
-        				   if ($scope.model.styleClass)  template += " class='" +$scope.model.styleClass + "'";
+        				   if ($scope.model.styleClass)  template += " class='svy-formcomponent " +$scope.model.styleClass + "'";
         				   template += "></div>";
         				   var div = $compile(template)($scope);
         				   div.append(elements);
         				   $element.append(div);
         			   }
+        			   // FIXME styleClass is not applied to responsive forms
         			   else $element.append(elements);
         		   }
         		   else {
@@ -56,6 +57,39 @@ angular.module('bootstrapcomponentsFormcomponent',['servoy']).directive('bootstr
         			  }
               	   });
         	   }
-        }
+        },
+		link:  function($scope, $element, $attrs) {
+			
+			var className = $scope.model.styleClass;
+			Object.defineProperty($scope.model, $sabloConstants.modelChangeNotifier, {
+						configurable: true,
+						value: function(property, value) {
+							switch (property) {
+							case "styleClass":
+								// TODO does not work for responsive forms
+								var div = $element.children()[0];
+								if (div) {
+									var wrapper = $(div);
+									if (wrapper.hasClass('svy-formcomponent')) {
+										if (className) wrapper.removeClass(className);
+										className = 'svy-formcomponent ' + value;
+										if (className) wrapper.addClass(className);
+									}
+								}
+								break;
+							}
+						}
+					});
+			var destroyListenerUnreg = $scope.$on("$destroy", function() {
+				destroyListenerUnreg();
+				delete $scope.model[$sabloConstants.modelChangeNotifier];
+			});
+			
+			// data can already be here, if so call the modelChange function so that it is initialized correctly.
+			var modelChangeFunction = $scope.model[$sabloConstants.modelChangeNotifier];
+			for (var key in $scope.model) {
+				modelChangeFunction(key,$scope.model[key]);
+			}
+		}
     }
 }])
