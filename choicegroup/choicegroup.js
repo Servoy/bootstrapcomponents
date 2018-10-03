65db88931d6a62de95b5d65777cdcdce677ce353
Costin Chiulan <costinchiulan@gmail.com>
2018-10-03 13:16:42 +0300

    SVY-12819 bootstrapcomponents-choicegroup valuelist empty value issue

--- choicegroup/choicegroup.js
+++ choicegroup/choicegroup.js
@@ -10,6 +10,8 @@ angular.module('bootstrapcomponentsChoicegroup',['servoy']).directive('bootstrap
       },
       link: function($scope, $element, $attrs) {
           
+          $scope.notNullOrEmpty = $utils.notNullOrEmpty  // adding it to the root scope doesn't fix the resolution of the comparator in the filter (in this directive). it has to be in local scope. TODO remove the need for this
+          var allowNullinc=0;
     	  $scope.selection= []
     	  
           $scope.$watch('model.dataProviderID', function() { 
@@ -20,6 +22,12 @@ angular.module('bootstrapcomponentsChoicegroup',['servoy']).directive('bootstrap
            	  $scope.model.valuelistID = [{realValue:1,displayValue:"Item1"},{realValue:2,displayValue:"Item2"},{realValue:3,displayValue:"Item3"}];
             }
             if(!$scope.model.valuelistID) return; // not loaded yet
+            if($scope.model.valuelistID.length > 0 && isValueListNull($scope.model.valuelistID[0])) allowNullinc=1;
+            else allowNullinc = 0;	
+            
+            console.log($scope.model.valuelistID);
+            console.log("AllowNullInc value is : " + allowNullinc);
+            
             setSelectionFromDataprovider();
           })
           
@@ -27,15 +35,30 @@ angular.module('bootstrapcomponentsChoicegroup',['servoy']).directive('bootstrap
            
     		if ($scope.model.inputType == 'radio')
     		{
-    			$scope.model.dataProviderID = $scope.model.valuelistID[$index].realValue;
+    			$scope.model.dataProviderID = $scope.model.valuelistID[$index+allowNullinc].realValue;
     		}
     		else
     		{
+                var checkedTotal = 0;
+                for(var i=0;i< $scope.selection.length ;i++){
+               	 if($scope.selection[i]==true) checkedTotal++;            	 
+                }
+
+               // prevent unselection of the last element if 'allow null' is not set                                          
+               if(checkedTotal==0 && allowNullinc ==0 && !$scope.model.findmode){
+                  $scope.selection[$index] = true;
+               }
     			$scope.model.dataProviderID = getDataproviderFromSelection()
+				if(checkedTotal==0 && allowNullinc ==0 && !$scope.model.findmode) return;
     		}	
             
 			$scope.svyServoyapi.apply('dataProviderID')
 			if($scope.handlers.onFocusLostMethodID) $scope.handlers.onFocusLostMethodID($event)
+          }
+          
+          function isValueListNull(item)
+          {
+              return (item.realValue == null || item.realValue =='') && item.displayValue=='';
           }
 
     	  function setSelectionFromDataprovider(){
@@ -44,16 +67,19 @@ angular.module('bootstrapcomponentsChoicegroup',['servoy']).directive('bootstrap
     		  var arr = $scope.model.dataProviderID.split ? $scope.model.dataProviderID.split('\n') : [$scope.model.dataProviderID];
     		  arr.forEach(function(element, index, array){
     			  for(var i=0;i<$scope.model.valuelistID.length;i++){
-    				  var item= $scope.model.valuelistID[i];
-    				  if(item.realValue && item.realValue==element) 
+    				  var item = $scope.model.valuelistID[i];
+                      if(item.realValue+''===element+'' && !isValueListNull(item))
     				  {
     					  if ($scope.model.inputType == 'radio')
     					  {
-    						  $scope.selection[i] = $scope.model.dataProviderID;
+    						  if(arr.length > 1)
+    							  $scope.selection = [];
+    						  else
+    						  	$scope.selection[i-allowNullinc] = item.realValue;
     					  }
     					  else
     					  {
-    						  $scope.selection[i] = true;
+    						  $scope.selection[i-allowNullinc] = true;
     					  }
     				  }
     			  }
@@ -63,8 +89,9 @@ angular.module('bootstrapcomponentsChoicegroup',['servoy']).directive('bootstrap
     	  function getDataproviderFromSelection(){
     		  var ret ="";
     		  $scope.selection.forEach(function(element, index, array){
-    			  if(element == true) ret+= $scope.model.valuelistID[index].realValue+'\n';
+    			  if(element == true) ret+= $scope.model.valuelistID[index+allowNullinc].realValue+'\n';
     		  });
+    		  ret = ret.replace(/\n$/, "");
     		  if(ret === "") ret =null
     		  return ret;
     	  }
