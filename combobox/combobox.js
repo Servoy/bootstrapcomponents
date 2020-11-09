21011bf27dde061795b52426d43d65bd27c0fd45
Paolo <paronne@servoy.com>
2020-11-09 17:31:56 +0200

    SVYX-129 Bts combobox shows &nbsp; as text Fix also TRUST_DATA_AS_HTML ignored

--- combobox/combobox.js
+++ combobox/combobox.js
@@ -128,12 +128,6 @@ angular.module('bootstrapcomponentsCombobox', ['servoy', 'ui.select'])
 		templateUrl: 'bootstrapcomponents/combobox/combobox.html'
 	};
 }])
-.filter('emptyOrNull', function () {
-	return function (item) {
-		if (item === null || item === '') {return '&nbsp;'; }
-		return item;
-	};
-})
 .filter('propertyFormattedFilter', function ($filter) {
 	return function(items, props, format, type) {
 		var out = [];
@@ -169,41 +163,5 @@ angular.module('bootstrapcomponentsCombobox', ['servoy', 'ui.select'])
 			out = items;
 		}
 		return out;
-	};
-})
-.filter('showDisplayValue', function () { // filter that takes the realValue as an input and returns the displayValue
-	return function (input, valuelist) {
-		var i = 0;
-		var realValue = input;
-		if (valuelist) {
-			if (input && input.hasOwnProperty("realValue")) {
-				realValue = input.realValue;
-			}
-			//TODO performance upgrade: change the valuelist to a hashmap so that this for loop is no longer needed. 
-			//maybe to something like {realValue1:displayValue1, realValue2:displayValue2, ...}
-			for (i = 0; i < valuelist.length; i++) {
-				if ((realValue + '') === (valuelist[i].realValue + '')) {
-					return valuelist[i].displayValue;
-				}
-			}
-			var hasRealValues = false;
-			for (var i = 0; i < valuelist.length; i++) {
-				var item = valuelist[i];
-				if (item.realValue != item.displayValue) {
-					hasRealValues = true;
-					break;
-				}
-			}
-			if (hasRealValues) {
-				var diplayValue = null;
-				// this then function will resolve right away if the value is already cached on the client.
-				valuelist.getDisplayValue(realValue).then(function(val){
-					diplayValue = val;
-				})
-				return diplayValue;
-			}
-			if (valuelist.length == 0) return null;
-		}
-		return input;
 	};
 });
