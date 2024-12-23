6a20ce52c7dc6e58ed1b43f21a1cff10c28842d0
Johan Compagner <jcompagner@servoy.com>
2024-12-23 12:20:34 +0100

    Revert "SVY-19829 Typeahead from bootstrap and strange selection - update"
    This reverts commit ef48b83c4d369dea5a58e33ddec8fdbb7a360f1f.

--- projects/bootstrapcomponents/src/typeahead/typeahead.ts
+++ projects/bootstrapcomponents/src/typeahead/typeahead.ts
@@ -34,8 +34,6 @@ export class ServoyBootstrapTypeahead extends ServoyBootstrapBasefield<HTMLInput
     valueToApply: { displayValue: string; realValue: any } = null;
 
     private realToDisplay: Map<any, string> = new Map();
-    
-    private displayValue: any;
 
     constructor(renderer: Renderer2, cdRef: ChangeDetectorRef, @Inject(DOCUMENT) doc: Document, protected formatService: FormattingService,
         windowService: WindowRefService) {
@@ -106,7 +104,7 @@ export class ServoyBootstrapTypeahead extends ServoyBootstrapBasefield<HTMLInput
 
         return merge(debouncedText$, inputFocus$, clicksWithClosedPopup$).pipe(switchMap(term => {
             if ((this.findmode || (!this.readOnly && this.editable)) && this.valuelistID) {
-                const promise = this.valuelistID.filterList(this.dataProviderID ? this.displayValue : term);
+                const promise = this.valuelistID.filterList(term)
                 this.lastFilteringPromise = promise;
                 promise.toPromise().finally(() => {
                     if (this.lastFilteringPromise == promise) {
@@ -209,7 +207,6 @@ export class ServoyBootstrapTypeahead extends ServoyBootstrapBasefield<HTMLInput
                 }
             }
         }
-        this.displayValue = result;
         return this.formatService.format(result, this.format, false);
     };
 
