0ab2c2f996948088635bbcdaf0604ef1cfa74a6e
lvostinar <lvostinar@servoy.com>
2026-02-16 12:14:16 +0200

    SVY-20863 Using a Format on a Field is causing Odd Behavior on New

--- components/projects/bootstrapcomponents/src/textbox/textbox.ts
+++ components/projects/bootstrapcomponents/src/textbox/textbox.ts
@@ -4,116 +4,128 @@ import { Format, FormatDirective, WindowRefService } from '@servoy/public';
 import { ServoyBootstrapBasefield } from '../bts_basefield';
 
 @Component({
-    selector: 'bootstrapcomponents-textbox',
-    templateUrl: './textbox.html',
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    standalone: false
+	selector: 'bootstrapcomponents-textbox',
+	templateUrl: './textbox.html',
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	standalone: false
 })
 export class ServoyBootstrapTextbox extends ServoyBootstrapBasefield<HTMLInputElement> {
 
-    readonly format = input<Format>(undefined);
-    readonly inputType = input<string>(undefined);
-    readonly autocomplete = input<string>(undefined);
-    readonly styleClassForEye = input<string>(undefined);
+	readonly format = input<Format>(undefined);
+	readonly inputType = input<string>(undefined);
+	readonly autocomplete = input<string>(undefined);
+	readonly styleClassForEye = input<string>(undefined);
 
-    readonly inputTypeChange = output<string>();
+	readonly inputTypeChange = output<string>();
 
-    readonly svyFormat = viewChild(FormatDirective);
-    
-    protected _inputType = signal<string>(undefined);
-    protected _autocomplete = signal<string>(undefined);
+	readonly svyFormat = viewChild(FormatDirective);
 
-    showPass = false;
-    classForEye = '';
+	protected _inputType = signal<string>(undefined);
+	protected _autocomplete = signal<string>(undefined);
 
-    constructor(renderer: Renderer2, cdRef: ChangeDetectorRef, @Inject(DOCUMENT) doc: Document, protected windowService: WindowRefService) {
-        super(renderer, cdRef, doc);
-    }
+	showPass = false;
+	classForEye = '';
 
-    svyOnInit() {
-        super.svyOnInit();
-        this._autocomplete.set(this.autocomplete());
-        this._inputType.set(this.inputType());
-        if(this.autocomplete() === 'off') {
-            this._autocomplete.set(this.windowService.nativeWindow.navigator.userAgent.match(/chrome/i) ? 'chrome-off' : 'off');
-        }
-        if (this.onActionMethodID()) {
-            this.renderer.listen(this.getFocusElement(), 'click', e => {
-                if (this._editable() === false) {
-                    this.onActionMethodID()(e);
-                }
-            });
-        }
-    }
+	constructor(renderer: Renderer2, cdRef: ChangeDetectorRef, @Inject(DOCUMENT) doc: Document, protected windowService: WindowRefService) {
+		super(renderer, cdRef, doc);
+	}
 
-    svyOnChanges(changes: SimpleChanges) {
-        super.svyOnChanges(changes);
-        if (changes.inputType) {
-            this.setInputType(this.inputType());
-        }
-        if (changes.styleClassForEye) {
+	svyOnInit() {
+		super.svyOnInit();
+		this._autocomplete.set(this.autocomplete());
+		this._inputType.set(this.inputType());
+		if (this.autocomplete() === 'off') {
+			this._autocomplete.set(this.windowService.nativeWindow.navigator.userAgent.match(/chrome/i) ? 'chrome-off' : 'off');
+		}
+		if (this.onActionMethodID()) {
+			this.renderer.listen(this.getFocusElement(), 'click', e => {
+				if (this._editable() === false) {
+					this.onActionMethodID()(e);
+				}
+			});
+		}
+	}
+
+	svyOnChanges(changes: SimpleChanges) {
+		super.svyOnChanges(changes);
+		if (changes.inputType) {
+			this.setInputType(this.inputType());
+		}
+		if (changes.styleClassForEye) {
 			this.addClassForEye();
 		}
-    }
+	}
 
-    onModelChange(newValue) {
-        // if format or invalid date, force dataprovider display with formated value / invalid date text
-        if(this.format() || (newValue && typeof newValue.getTime === 'function' && isNaN(newValue.getTime()))) {
-            this.svyFormat().writeValue(newValue);
-        } 
-        this._dataProviderID.set(newValue);
-        
-        if (this.isDateType()) {
+	onModelChange(newValue) {
+		// if format or invalid date, force dataprovider display with formated value / invalid date text
+		if (this.format() || (newValue && typeof newValue.getTime === 'function' && isNaN(newValue.getTime()))) {
+			this.svyFormat().writeValue(newValue);
+		}
+		this._dataProviderID.set(newValue);
+
+		if (this.isDateType()) {
 			this.pushUpdate();
 		}
-    }
+	}
 
-    setInputType(inputType: string) {
-        const types = ['text', 'password', 'password-with-eye', 'email', 'tel', 'date', 'time', 'datetime-local', 'month', 'week', 'number', 'color', 'search', 'url'];
+	setInputType(inputType: string) {
+		const types = ['text', 'password', 'password-with-eye', 'email', 'tel', 'date', 'time', 'datetime-local', 'month', 'week', 'number', 'color', 'search', 'url'];
 
-        if (types.indexOf(inputType) > -1) {
-            const inputTypeValue = this._inputType();
-            if (inputTypeValue !== inputType) {
-                this._inputType.set(inputType);
-                this.inputTypeChange.emit(inputType);
-            }
-            const dp = this._dataProviderID();
-            if (dp) {
-                this.svyFormat().writeValue(dp);
-            }
-            return true;
-        } else {
-            return false;
-        }
-    }
+		if (types.indexOf(inputType) > -1) {
+			const inputTypeValue = this._inputType();
+			if (inputTypeValue !== inputType) {
+				this._inputType.set(inputType);
+				this.inputTypeChange.emit(inputType);
+			}
+			const dp = this._dataProviderID();
+			if (dp) {
+				this.svyFormat().writeValue(dp);
+			}
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    showHidePass() {
-	    this.showPass = !this.showPass;
-	    this.addClassForEye();
-    }
+	showHidePass() {
+		this.showPass = !this.showPass;
+		this.addClassForEye();
+	}
 
-    private addClassForEye() {
-        let [mainClass, openClass, closeClass] = ['fa', 'fa-eye', 'fa-eye-slash'];
-        const styleClassForEye = this.styleClassForEye();
-        if (styleClassForEye) {
-            const classes = styleClassForEye.split(' ');
-            if (classes.length === 3) {
-                [mainClass, openClass, closeClass] = classes;
-            } else if (classes.length === 2) {
-                mainClass = '';
-                [openClass, closeClass] = classes;
-            }
-        }
+	private addClassForEye() {
+		let [mainClass, openClass, closeClass] = ['fa', 'fa-eye', 'fa-eye-slash'];
+		const styleClassForEye = this.styleClassForEye();
+		if (styleClassForEye) {
+			const classes = styleClassForEye.split(' ');
+			if (classes.length === 3) {
+				[mainClass, openClass, closeClass] = classes;
+			} else if (classes.length === 2) {
+				mainClass = '';
+				[openClass, closeClass] = classes;
+			}
+		}
 
-        this.classForEye = mainClass + ' ' + closeClass;
+		this.classForEye = mainClass + ' ' + closeClass;
 
-        if (this.showPass) {
-            this.classForEye = mainClass + ' ' + openClass;
-        }
-    }
-	
+		if (this.showPass) {
+			this.classForEye = mainClass + ' ' + openClass;
+		}
+	}
+
 	isDateType() {
 		const types = ['date', 'time', 'datetime-local', 'month', 'week'];
 		return types.includes(this.inputType());
+	}
+
+	requestFocus(mustExecuteOnFocusGainedMethod: boolean) {
+		if (this.format() && this.format().isMask && this.dataProviderID() != (this.getFocusElement() as HTMLInputElement).value) {
+			// wait for ui value to be updated before focus, otherwise mask library will be initialized with the old value and not work properly
+			setTimeout(() => {
+				super.requestFocus(mustExecuteOnFocusGainedMethod);
+			});
+		}
+		else {
+			super.requestFocus(mustExecuteOnFocusGainedMethod);
+		}
 	}
 }
